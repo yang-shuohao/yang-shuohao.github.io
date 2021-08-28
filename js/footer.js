74478f2ab9b1b0a64583530bb166cfeafc4c1329
yang-shuohao <2499134263@qq.com>
2021-08-28 17:23:41 +0800

    Site updated: 2021-08-28 17:23:38

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 // 动态心跳
 $(document).ready(function(e){
-    $('.copyright').html('©2021 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 花猪');
+    $('.copyright').html('©2021 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By yangshuohao');
 })
 
 $(document).ready(function(e){
