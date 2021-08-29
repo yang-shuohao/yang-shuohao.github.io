014e663e9a6c1040a010c17abad2f8bbe247f6f4
yang-shuohao <2499134263@qq.com>
2021-08-29 14:27:06 +0800

    Site updated: 2021-08-29 14:27:02

--- js/updown.js
+++ js/updown.js
@@ -1,7 +1,7 @@
 $(function(){
  
 /*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://img-blog.csdnimg.cn/b65d0b66eeb7486c9f02544a5449b556.gif#pic_center' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 
