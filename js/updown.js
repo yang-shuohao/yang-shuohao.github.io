98ce9445d52ef5e4c2b5bd37ba7b3b891c08d04a
yang-shuohao <2499134263@qq.com>
2021-08-29 14:40:37 +0800

    Site updated: 2021-08-29 14:40:32

--- js/updown.js
+++
@@ -1,8 +0,0 @@
-$(function(){
- 
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://img-blog.csdnimg.cn/e01367cf4e1c497eb671574dd2499e43.gif#pic_center' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
-
-})
