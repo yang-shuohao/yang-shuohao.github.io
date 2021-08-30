c469fa6a7e85da1369fbfb66c11a390cffa613ba
yang-shuohao <2499134263@qq.com>
2021-08-30 21:28:46 +0800

    Site updated: 2021-08-30 21:28:41

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -19,7 +19,7 @@
          TagCanvas.textHeight = 25;
          TagCanvas.outlineColour = '#E2E1D1';
          TagCanvas.maxSpeed = 0.2;
-         TagCanvas.freezeActive = true;
+         TagCanvas.freezeActive = false;
          TagCanvas.outlineMethod = 'block';
          TagCanvas.minBrightness = 0.2;
          TagCanvas.depth = 0.92;
